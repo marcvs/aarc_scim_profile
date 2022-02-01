73e3e080713aedbec8f091e636b5942129a3993a
Marcus <marcus@hardt-it.de>
2022-02-01 18:09:58 +0100

    add support for comments and unclear definitions

--- make_parseable.sh
+++ make_parseable.sh
@@ -6,6 +6,8 @@ OUPUT="AARC_Schema_Parseeble.json"
 cat AARC_Schema.json \
     | grep -vE "(saml_attribute_name|saml_attribute_urn|status)" \
     | sed s_\ //.*__ \
+    | sed s/\"unclear.*\"/true/ \
+    | sed s/\"to\ be\ evaluated\"/true/ \
     > AARC_Schema_Parseable.json
 #cat ${INTPUT} \
 #    | grep -vE "(saml_attribute_name|saml_attribute_urn|status)" \
